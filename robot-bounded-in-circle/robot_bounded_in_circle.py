284c633e7bb87c792062586c2ab8795b2dfd9a1b
Carlos Moro <cmoro@homeaway.com>
2021-07-01 11:45:40 -0700

    adding accepted solution for `robot-bounded-in-circle` problem

--- robot-bounded-in-circle/robot_bounded_in_circle.py
+++ robot-bounded-in-circle/robot_bounded_in_circle.py
@@ -5,8 +5,97 @@ class Solution:
     def isRobotBounded(self, instructions: str) -> bool:
 
         solution = None
+
+        # initializing initial position and distance to base
+        position = [0,0]
+        distance = 0
+
+        # initializing direction
+        direction = "north"
+
+        # maximum number of iterations
+        max_iterations = 4
+
+        for i in range(max_iterations):
+            # iterate over instructions and execute them one by one
+            print('****** iteration: {} ******'.format(i+1))
+            for instruction in instructions:
+                print('-->Instruction: {}'.format(instruction))
+                position, direction = self.options[instruction](self, position, direction)
+                print('New position: {}'.format(position))
+                print('New direction: {}'.format(direction))
+
+            if position == [0,0]:
+                # returned to base so this is definitely a circular instruction
+                return True
+
+            #distance = self.distance_to_base(position)
+            #print('distance to base after iteration: {}'.format(distance))
+
+
         return solution
 
+    def distance_to_base(self, position):
+        import numpy as np
+
+        # intializing points in numpy arrays
+        point1 = np.array((0, 0))
+        point2 = np.array((position[0], position[1]))
+
+        # finding sum of squares
+        sum_sq = np.sum(np.square(point2 - point1))
+
+        # Doing squareroot and printing Euclidean distance
+        distance = np.sqrt(sum_sq)
+        print('Distance to base: {}'.format(distance))
+        return distance
+
+
+    def G(self, position, direction):
+        # implement instruction G
+        print('instruction G was executed')
+        if direction == "north":
+            return [position[0],position[1]+1], direction
+        elif direction == "south":
+            return [position[0],position[1]-1], direction
+        elif direction == "west":
+            return [position[0]-1,position[1]], direction
+        elif direction == "east":
+            return [position[0]+1,position[1]], direction
+
+
+    def L(self, position, direction):
+        # implement instruction L
+        print('instruction L was executed')
+        if direction == "north":
+            return [position[0],position[1]], "west"
+        elif direction == "south":
+            return [position[0],position[1]], "east"
+        elif direction == "west":
+            return [position[0],position[1]], "south"
+        elif direction == "east":
+            return [position[0],position[1]], "north"
+
+
+    def R(self, position, direction):
+        # implement instruction R
+        print('instruction R was executed')
+        if direction == "north":
+            return [position[0],position[1]], "east"
+        elif direction == "south":
+            return [position[0],position[1]], "west"
+        elif direction == "west":
+            return [position[0],position[1]], "north"
+        elif direction == "east":
+            return [position[0],position[1]], "south"
+
+    options = {
+        "G" : G,
+        "L" : L,
+        "R" : R
+    }
+
+########################################### END OF SOLUTION CODE #######################################################
 
 
 # Definition for a binary tree node.
--- template-solution/template_solution.py
+++ template-solution/template_solution.py
@@ -7,6 +7,7 @@ class Solution:
         solution = None
         return solution
 
+########################################### END OF SOLUTION CODE #######################################################
 
 
 # Definition for a binary tree node.
