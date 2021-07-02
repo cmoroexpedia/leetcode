d5c6e6575f23859fb666e94911d1798532b0e4a9
Carlos Moro <cmoro@homeaway.com>
2021-07-02 15:49:54 -0700

    adding amazon interview question

---
+++ amazon-interview/amazon_interview.py
@@ -0,0 +1,236 @@
+import json
+
+
+class Solution:
+
+    def lengthOfLongestSubstring(self, input):
+
+
+        # dictionary to keep track of chars seen
+        seen = {}
+
+        # longest substring so far
+        longest_substring = ""
+
+        # last repetition seen
+        subsring_start = 0
+
+        for index,char in enumerate(input):
+            print('index: {}, char: {}'.format(index,char))
+            if char in seen:
+                print('seen: {}'.format(char))
+                substring = input[subsring_start:index]
+                print('Substring: {}'.format(substring))
+                if len(substring) > len(longest_substring):
+                    longest_substring = substring
+                if char == input[index-1]:
+                    subsring_start = index
+                else:
+                    subsring_start = index - 1
+
+            seen[char] = index
+
+        return longest_substring
+
+
+# this is the solution I came up during the interview, but it's not correct
+class SolutionWrong:
+
+    def lengthOfLongestSubstring(self, input):
+
+
+        # dictionary to keep track of chars seen
+        seen = {}
+
+        # longest substring so far
+        longest_substring = ""
+
+        # last repetition seen
+        last_repeated_char = 0
+
+        for index,char in enumerate(input):
+
+            if char in seen:
+                substring = input[last_repeated_char:index]
+                if len(substring) > len(longest_substring):
+                    longest_substring = substring
+                last_repeated_char = seen[char]
+
+            seen[char] = index
+
+        return longest_substring
+
+########################################### END OF SOLUTION CODE #######################################################
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+# Other code snippets
+def stringToIntegerList(input):
+    return json.loads(input)
+
+
+def stringToListNode(input):
+    # Generate list from the input
+    numbers = stringToIntegerList(input)
+
+    # Now convert that list into linked list
+    dummyRoot = ListNode(0)
+    ptr = dummyRoot
+    for number in numbers:
+        ptr.next = ListNode(number)
+        ptr = ptr.next
+
+    ptr = dummyRoot.next
+    return ptr
+
+
+def integerListToString(nums, len_of_list=None):
+    if not len_of_list:
+        len_of_list = len(nums)
+    return json.dumps(nums[:len_of_list])
+
+
+def listNodeToString(node):
+    if not node:
+        return "[]"
+
+    result = ""
+    while node:
+        result += str(node.val) + ", "
+        node = node.next
+    return "[" + result[:-2] + "]"
+
+
+def stringToBool(input):
+    return json.dumps(input)
+
+
+def treeNodeToString(root):
+    if not root:
+        return "[]"
+    output = ""
+    queue = [root]
+    current = 0
+    while current != len(queue):
+        node = queue[current]
+        current = current + 1
+
+        if not node:
+            output += "null, "
+            continue
+
+        output += str(node.val) + ", "
+        queue.append(node.left)
+        queue.append(node.right)
+    return "[" + output[:-2] + "]"
+
+
+def stringToTreeNode(input):
+    input = input.strip()
+    input = input[1:-1]
+    if not input:
+        return None
+
+    inputValues = [s.strip() for s in input.split(',')]
+    root = TreeNode(int(inputValues[0]))
+    nodeQueue = [root]
+    front = 0
+    index = 1
+    while index < len(inputValues):
+        node = nodeQueue[front]
+        front = front + 1
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            leftNumber = int(item)
+            node.left = TreeNode(leftNumber)
+            nodeQueue.append(node.left)
+
+        if index >= len(inputValues):
+            break
+
+        item = inputValues[index]
+        index = index + 1
+        if item != "null":
+            rightNumber = int(item)
+            node.right = TreeNode(rightNumber)
+            nodeQueue.append(node.right)
+    return root
+
+
+def stringToString(input):
+    return json.loads(input)
+
+
+def prettyPrintTree(node, prefix="", isLeft=True):
+    if not node:
+        print("Empty Tree")
+        return
+
+    if node.right:
+        prettyPrintTree(node.right, prefix + ("│   " if isLeft else "    "), False)
+
+    print(prefix + ("└── " if isLeft else "┌── ") + str(node.val))
+
+    if node.left:
+        prettyPrintTree(node.left, prefix + ("    " if isLeft else "│   "), True)
+
+
+def prettyPrintLinkedList(node):
+    while node and node.next:
+        print(str(node.val) + "->", end='')
+        node = node.next
+
+    if node:
+        print(node.val)
+    else:
+        print("Empty LinkedList")
+
+
+def intToString(input):
+    if input is None:
+        input = 0
+    return str(input)
+
+
+def boolToString(input):
+    if input is None:
+        input = False
+    return str(input)
+
+
+def main():
+    import sys
+    def readlines():
+        for line in sys.stdin:
+            yield line.strip('\n')
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            print("********************** start ************************")
+            input = stringToString(line)
+            print("input: " + str(input))
+
+            ret = Solution().lengthOfLongestSubstring(input)
+
+            print("output: " + ret)
+        except StopIteration:
+            break
+
+if __name__ == '__main__':
+    main()
